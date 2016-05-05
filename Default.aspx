b9431154a6645b27eda1cf777ee7f8befc5ae390
mcdavis19 <mcd6@uw.edu>
2016-05-05 11:56:50 -0700

    Added files via upload

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div><!--Artists-->
+        <h2>Artists</h2>
+        <asp:GridView ID="gvArtists" runat="server"></asp:GridView>
+    </div>
+
+    <div><!--Venues-->
+        <h2>Venues</h2>
+        <asp:GridView ID="gvVenues" runat="server"></asp:GridView>
+    </div>
+
+    <div><!--Shows-->
+        <h2>Shows</h2>
+        <asp:GridView ID="gvShows" runat="server"></asp:GridView>
+    </div>
+    
+    <br /><!--Shows by Venue-->
+    <div id="ShowsByVenue">
+        <h2>Shows by Venue</h2>
+        <asp:DropDownList ID="VenuesDropDown" runat="server"></asp:DropDownList>
+        <asp:gridview id="gvShowsByVenue" runat="server"></asp:gridview>
+        <asp:button id="btnGetShowsByVenue" runat="server" text="Get Shows" onclick="btngetshowsbyvenue_click" Width="90px" />
+    </div>
+
+    <br /><!--Shows by Artist-->
+    <div id="ShowsByArtist">
+        <h2>Shows by Artist</h2>
+        <asp:DropDownList ID="ArtistsDropDown" runat="server"></asp:DropDownList>
+        <asp:GridView ID="gvShowsByArtist" runat="server"></asp:GridView>
+        <asp:Button ID="btnGetShowsByArtist" runat="server" Text="Get Shows" OnClick="btnGetShowsByArtist_Click" Width="90px" />
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerServiceRef.ShowTrackerServiceClient ShowTracker = new ShowTrackerServiceRef.ShowTrackerServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FillGridView(gvArtists, ShowTracker.GetArtists());
+            FillGridView(gvVenues, ShowTracker.GetVenues());
+            FillGridView(gvShows, ShowTracker.GetShows());
+
+            LoadArtistsDropDown();
+            LoadVenuesDropDown();
+        }
+    }
+    protected void FillGridView(GridView gv, object[] list)
+    {
+        gv.DataSource = list;
+        gv.DataBind();
+    }
+    protected void LoadVenuesDropDown()
+    {
+        string[] venues = ShowTracker.GetVenues();
+        VenuesDropDown.DataSource = venues;
+        VenuesDropDown.DataBind();
+        VenuesDropDown.Items.Insert(0, "Pick a Venue");
+        VenuesDropDown.Items.Insert(1, "---------");
+    }
+    protected void LoadArtistsDropDown()
+    {
+        string[] artists = ShowTracker.GetArtists();
+        ArtistsDropDown.DataSource = artists;
+        ArtistsDropDown.DataBind();
+        ArtistsDropDown.Items.Insert(0, "Pick an Artist");
+        ArtistsDropDown.Items.Insert(1, "---------");
+    }
+
+    protected void btnGetShowsByArtist_Click(object sender, EventArgs e)
+    {
+        FillGridView(gvShowsByArtist, ShowTracker.GetShowsByArtist(ArtistsDropDown.SelectedValue));
+    }
+
+    protected void btngetshowsbyvenue_click(object sender, EventArgs e)
+    {
+        FillGridView(gvShowsByVenue, ShowTracker.GetShowsByVenue(VenuesDropDown.SelectedValue));
+    }
+}
